408ffa933f68eb8aa0a6d3c4115dbc4daf9c7020
utkarsh-creator <utkarshtyagi959@gmail.com>
2023-10-13 00:08:15 +0530

    initial commit

--- ftpdownload/download.php
+++ ftpdownload/download.php
@@ -1,36 +1,38 @@
 <?php
 
-$ftp_server = "your host name";
-$ftp_username = "your username";
-$ftp_password = 'your password';
-$download_dir = "/htdocs/"; // eg "/htdocs/","/" the path on the remote site from where you want to download the file.
-$file_name = $_POST["file"];
+$ftp_server = "your_ftp_server";
+$ftp_username = "your_ftp_username";
+$ftp_password = 'your_ftp_password';
+$download_dir = "/htdocs/";
+$file_name = isset($_POST["file"]) ? $_POST["file"] : null;
 
+if ($file_name !== null) {
+    $conn_id = ftp_connect($ftp_server);
 
-$conn_id = ftp_connect($ftp_server);
+    if ($conn_id) {
+        $login_result = ftp_login($conn_id, $ftp_username, $ftp_password);
 
-if ($conn_id) {
-    $login_result = ftp_login($conn_id, $ftp_username, $ftp_password);
+        if ($login_result) {
+            ftp_pasv($conn_id, true); // Enable passive mode
 
-    if ($login_result) {
-        ftp_pasv($conn_id, true); // Enable passive mode
+            $remote_file_path = $download_dir . $file_name;
+            $local_file_path = "C:/Users/your-username/Desktop/download/" . $file_name;
 
-        $remote_file_path = $download_dir . $file_name;
-        $local_file_path = "C:/Users/your-username/Desktop/download/" . $file_name; //path where you want to save your downloaded file from server
+            $download_result = ftp_get($conn_id, $local_file_path, $remote_file_path, FTP_BINARY);
 
-        $download_result = ftp_get($conn_id, $local_file_path, $remote_file_path, FTP_BINARY);
-
-        if ($download_result) {
-            echo "File downloaded successfully.";
+            if ($download_result) {
+                echo "File downloaded successfully.";
+            } else {
+                echo "Download failed.";
+            }
         } else {
-            echo "Download failed.";
+            echo "FTP login failed.";
         }
-    } else {
-        echo "FTP login failed.";
-    }
 
-    ftp_close($conn_id);
+        ftp_close($conn_id);
+    } else {
+        echo "Could not connect to FTP server.";
+    }
 } else {
-    echo "Could not connect to FTP server.";
+    echo "Invalid file name.";
 }
-?>
